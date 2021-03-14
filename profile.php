8ea9437daeca94caf998625470fe9185c3020493
AnugerahM <anugerahgz09@gmail.com>
2021-03-14 14:45:22 +0800

    commit pertama: menambahkan semua fail hasil praktikum pertemuan 1-3

---
+++ profile.php
@@ -0,0 +1,58 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/Nugrah.jpg" alt="Nugrah" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>NPM :</strong></td>
+                        <td align="left" width="50%">17630067</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studi :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Anugerah Muhammad</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">Banjarmasin, 15 Agustus 1999</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">0878 1500 3138</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">anugerahgz09@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.facebook.com/anugerah.chelsea" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/036-facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="https://www.instagram.com/anugerahm__/" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                             <a href="https://www.facebook.com/nugrahjisoo" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/fbgaming.png" width="30" alt="fbgaming">
+                            </a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Team Esport :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="https://www.instagram.com/ez.victory/" target="_blank" style="text-decoration:none;">
+                                <img src="img/svg/029-instagram.svg" width="30" alt="instagram">
+                            </a>
+                           
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
